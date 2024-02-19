e7c157eb086f121450625c6c9429455c287ce9d1
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-02-19 21:25:49 +0200

    Add project files.

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+impar
+ In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+ Google :even and odd number*/
+
+Console.WriteLine("Introduceti numarul");
+int Numar = Convert.ToInt32(Console.ReadLine());
+if (Numar %2==0)
+{
+    Console.WriteLine("Par");
+}
+else {Console.WriteLine("Impar");
+}
